4b559cb667c53e35b702b655a0d72b028c26fd4d
shivamraut101 <shivamkumarraut12@gmail.com>
2023-12-20 15:07:41 +0530

    Pages - Homepage

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Homepage(){
+    return(
+        <>
+        <input type="email" placeholder="Enter Your Email..." />
+        <input type="text" placeholder="Enter Your Room Code..." />
+        <button>Enter Room</button>
+        </>
+    )
+}
+
+export default Homepage;
