bd74232ec779ffa100ef4432cb4fb5b6e7caad3d
shivamraut101 <shivamkumarraut12@gmail.com>
2023-12-21 19:22:46 +0530

    fixed CORS and modification

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const {Server} = require("socket.io");
 
-const io = new Server();
+const io = new Server({
+    cors: true
+});
 const app = express();
 
 app.use(bodyparser.json());
@@ -10,10 +12,12 @@ app.use(bodyparser.json());
 const emailToSocketMapping = new Map();
 
 io.on("connection", (socket) => {
+    console.log("new socket", socket.id)
     socket.on("join-room", (data)=>{
         const { roomId, emailId } = data;
         console.log("User ", emailId, "Joined Room ", roomId);
         emailToSocketMapping.set(emailId,socket.id);
+        socket.emit("joined-room", { roomId })
         socket.join(roomId);
         socket.broadcast.to(roomId).emit("user-joined", { emailId });
     })
